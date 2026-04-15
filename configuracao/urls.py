a3192259aaf4e612df29fb1284f3f164349cb173
LUIZ AUGUSTO MANSO DO NASCIMENTO <luizaugusto2006@gmail.com>
2026-04-15 08:32:33 -0300

    Ajuste nas urls para exibir fotos em producao

--- configuracao/urls.py
+++ configuracao/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('', home, name='home'),
 ]
 
-# Este bloco abaixo é o que faz a mágica das fotos aparecerem
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # ISSO É O QUE FALTA PARA A RENDER ACHAR AS FOTOS NO DEPLOY
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
