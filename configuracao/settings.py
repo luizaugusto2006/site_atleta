a924a6a678a408934b61f9dc8de5f0484c50f1a3
LuizAugusto <luizaugusto2006@gmail.com>
2026-06-28 18:15:00 -0300

    Downgrade Django para 5.1.6 LTS (compatível Python 3.10 do PythonAnywhere)

--- configuracao/settings.py
+++ configuracao/settings.py
@@ -84,8 +84,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Pasta onde o Django reunirá o CSS no Deploy
 
-# Configuração para o WhiteNoise servir arquivos estáticos de forma eficiente
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    'default': {'BACKEND': 'django.core.files.storage.FileSystemStorage'},
+    'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'},
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
