83f8afef15b664fbe19e97d45f274b3b76fbfbcb
LuizAugusto <luizaugusto2006@gmail.com>
2026-06-28 18:54:50 -0300

    Substitui serve do django por view propria (FileResponse) para servir midia

--- configuracao/urls.py
+++ configuracao/urls.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
-from django.urls import path, re_path # Adicionamos re_path aqui
-from core.views import home
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve # Importação essencial para produção
+from django.urls import path, re_path
+from core.views import home, media_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-]
-
-# Esta configuração abaixo substitui o seu IF/ELSE anterior
-# Ela funciona tanto no seu PC quanto na Render
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', media_serve, name='media_serve'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
+import os
 from django.shortcuts import render
+from django.http import FileResponse, Http404
+from django.conf import settings
 from .models import Atleta
 
 def home(request):
-    # Buscamos o primeiro atleta cadastrado no banco
-    atleta = Atleta.objects.first() 
+    atleta = Atleta.objects.first()
     return render(request, 'index.html', {'atleta': atleta})
+
+def media_serve(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if not os.path.exists(file_path):
+        raise Http404("Arquivo não encontrado")
+    return FileResponse(open(file_path, 'rb'))
